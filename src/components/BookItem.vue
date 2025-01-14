1e9e273f02a8e604816c1f8429e04b660371cc00
denosaur98 <denischurchill1998@gmail.com>
2025-01-14 22:07:21 +0300

    верстка BookItem

---
+++ src/components/BookItem.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="book-item">
+    <div class="item__header">
+      <h1 class="header__title">Как разговаривать с кем угодно, когда угодно, где угодно</h1>
+      <button class="header__edit">
+        <img src="../assets/icons/edit.svg">
+      </button>
+    </div>
+    <div class="item__info">
+      <p class="info__element">Ларри Кинг</p>
+      <p class="info__element">2011</p>
+      <p class="info__element">Научпоп</p>
+    </div>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+.book-item {
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  background: rgb(245, 246, 246);
+  border-radius: 16px;
+  gap: 10px;
+  padding: 15px;
+
+  .item__header {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    width: 100%;
+
+    @media (max-width: 600px) {
+      align-items: flex-start;
+    }
+
+    .header__title {
+      color: rgb(15, 23, 42);
+      font-size: 16px;
+      font-weight: 400;
+      line-height: 120%;
+      letter-spacing: 0%;
+      text-align: left;
+    }
+
+    .header__edit {
+      cursor: pointer;
+      border: none;
+      background: none;
+      margin-left: 10px;
+    }
+  }
+
+  .item__info {
+    display: flex;
+    align-items: center;
+    gap: 15px;
+
+    .info__element {
+      color: rgb(112, 119, 134);
+      font-size: 14px;
+      font-weight: 400;
+      line-height: 150%;
+      letter-spacing: 0px;
+      text-align: left;
+    }
+  }
+}
+</style>
--- src/components/BooksList.vue
+++ src/components/BooksList.vue
@@ -1,120 +1,140 @@
 <template>
-  <div class="books-list">
-    <div class="list__header-wrapper">
-      <div class="header__search-wrapper">
-        <img src="../assets/icons/logo.svg" class="search__logo">
-        <div class="search__item-wrapper">
-          <img src="../assets/icons/search.svg" class="item__icon">
-          <input type="text" placeholder="Найти ту самую книгу" class="search__item">
+  <div class="books-list-wrapper">
+    <div class="books-list">
+      <div class="list__header-wrapper">
+        <div class="header__search-wrapper">
+          <img src="../assets/icons/logo.svg" class="search__logo">
+          <div class="search__item-wrapper">
+            <img src="../assets/icons/search.svg" class="item__icon">
+            <input type="text" placeholder="Найти ту самую книгу" class="search__item">
+          </div>
+        </div>
+        <div class="header__info-wrapper">
+          <h1 class="info__title">
+            Книги в каталоге
+            <span class="info__count">3</span>
+          </h1>
+          <button class="info__add-book">
+            <img src="../assets/icons/add.svg">
+            Добавить книгу
+          </button>
         </div>
       </div>
-      <div class="header__info-wrapper">
-        <h1 class="info__title">
-          Книги в каталоге
-          <span class="info__count">3</span>
-        </h1>
-        <button class="info__add-book">
-          <img src="../assets/icons/add.svg">
-          Добавить книгу
-        </button>
-      </div>
+    </div>
+    <div class="list__place">
+      <BookItem />
     </div>
   </div>
 </template>
 
 <script setup>
-
+import BookItem from './BookItem';
 </script>
 
 <style lang="scss" scoped>
-.books-list {
+.books-list-wrapper {
   display: flex;
   flex-direction: column;
   width: 100%;
-  padding: 15px 40px;
-  background: rgb(245, 246, 246);
 
-  .list__header-wrapper {
+  .books-list {
     display: flex;
     flex-direction: column;
-    gap: 30px;
     width: 100%;
+    padding: 15px 40px;
+    background: rgb(245, 246, 246);
 
-    .header__search-wrapper {
+    .list__header-wrapper {
       display: flex;
-      align-items: center;
+      flex-direction: column;
+      gap: 30px;
       width: 100%;
-      gap: 25px;
 
-      .search__logo {
-        width: auto;
-        height: auto;
-      }
-
-      .search__item-wrapper {
+      .header__search-wrapper {
         display: flex;
         align-items: center;
         width: 100%;
-        background: #fff;
-        border-radius: 8px;
+        gap: 25px;
 
-        .item__icon {
-          margin: 0 0 0 12px;
+        .search__logo {
+          width: auto;
+          height: auto;
         }
 
-        .search__item {
+        .search__item-wrapper {
+          display: flex;
+          align-items: center;
           width: 100%;
-          border: none;
+          background: #fff;
+          border-radius: 8px;
+
+          .item__icon {
+            margin: 0 0 0 12px;
+          }
+
+          .search__item {
+            width: 100%;
+            border: none;
+            font-size: 14px;
+            font-weight: 400;
+            line-height: 150%;
+            letter-spacing: 0px;
+            text-align: left;
+            padding: 10px 12px;
+            border-radius: 8px;
+
+            &::placeholder {
+              color: rgb(112, 119, 134);
+            }
+          }
+        }
+      }
+
+      .header__info-wrapper {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        width: 100%;
+
+        .info__title {
+          color: rgb(15, 23, 42);
+          font-size: 20px;
+          font-weight: 600;
+          line-height: 100%;
+          letter-spacing: 0%;
+          text-align: left;
+
+          .info__count {
+            color: rgb(112, 119, 134);
+            margin-left: 10px;
+          }
+        }
+
+        .info__add-book {
+          cursor: pointer;
+          display: flex;
+          align-items: center;
+          padding: 10px 12px;
+          border-radius: 8px;
+          background: rgb(28, 125, 65);
+          color: rgb(255, 255, 255);
           font-size: 14px;
           font-weight: 400;
           line-height: 150%;
           letter-spacing: 0px;
           text-align: left;
-          padding: 10px 12px;
-          border-radius: 8px;
-
-          &::placeholder {color: rgb(112, 119, 134);}
+          border: none;
+          gap: 5px;
         }
       }
     }
+  }
 
-    .header__info-wrapper {
-      display: flex;
-      align-items: center;
-      justify-content: space-between;
-      width: 100%;
-
-      .info__title {
-        color: rgb(15, 23, 42);
-        font-size: 20px;
-        font-weight: 600;
-        line-height: 100%;
-        letter-spacing: 0%;
-        text-align: left;
-
-        .info__count {
-          color: rgb(112, 119, 134);
-          margin-left: 10px;
-        }
-      }
-
-      .info__add-book {
-        cursor: pointer;
-        display: flex;
-        align-items: center;
-        padding: 10px 12px;
-        border-radius: 8px;
-        background: rgb(28, 125, 65);
-        color: rgb(255, 255, 255);
-        font-size: 14px;
-        font-weight: 400;
-        line-height: 150%;
-        letter-spacing: 0px;
-        text-align: left;
-        border: none;
-        gap: 5px;
-      }
-    }
+  .list__place {
+    display: flex;
+    flex-direction: column;
+    padding: 15px 40px;
+    gap: 15px;
   }
 }
 </style>
